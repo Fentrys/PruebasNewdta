fa634573969df3956a8a05eb0f0a104c9a1e7236
Bastiaans cloud <luismanuelpd@gmail.com>
2019-11-29 05:26:22 -0400

    Registrar todas las app

--- apps/solicitud/admin.py
+++ apps/solicitud/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.solicitud.models import Solicitudes, Contactos
 
 # Register your models here.
+admin.site.register(Solicitudes)
+admin.site.register(Contactos)
